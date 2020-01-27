8ca20d41fb6fa504fd0b1543cac32d6aec21fe3c
charadri <kirillkulikov307@gmail.com>
2020-01-27 20:23:24 +0300

    step 3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,9 @@
+from ..cli import welcome_user
+#from brain_games import cli
+#import ..cli
 def main():
     print('Welcome to the Brain Games!')
+    welcome_user()
 
 if __name__ == '__main__':
     main()
